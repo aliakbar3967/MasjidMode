77d245ddad9f70b2c2ab14ffb13af4c82e94ec98
Sabuj Islam <sabuj0338@gmail.com>
2021-03-10 18:18:29 +0600

    updates on home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,118 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_foreground_service_plugin/flutter_foreground_service_plugin.dart';
-import 'package:peace_time/model/schedule.dart';
-import 'package:peace_time/screens/create_schedule_screen.dart';
-import 'package:peace_time/screens/home_screen.dart';
-import 'package:peace_time/widgets/checkbox.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 
 /// Global [SharedPreferences] object.
-SharedPreferences prefs;
+// SharedPreferences prefs;
 
-void main() {
-  runApp(RootScreen());
+// void main() {
+//   runApp(RootScreen());
+// }
+
+// class RootScreen extends StatefulWidget {
+//   @override
+//   _RootScreenState createState() => _RootScreenState();
+// }
+
+// class _RootScreenState extends State<RootScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Peace Time',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: HomeScreen(),
+//     );
+//   }
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+
+  _MyAppState createState() => _MyAppState();
 }
 
-class RootScreen extends StatefulWidget {
+class _MyAppState extends State<MyApp> {
+  bool selectingmode = false;
+  List<Paint> paints = <Paint>[
+    Paint(1, 'Red', Colors.red),
+    Paint(2, 'Blue', Colors.blue),
+    Paint(3, 'Green', Colors.green),
+    Paint(4, 'Lime', Colors.lime),
+    Paint(5, 'Indigo', Colors.indigo),
+    Paint(6, 'Yellow', Colors.yellow)
+  ];
   @override
-  _RootScreenState createState() => _RootScreenState();
-}
 
-class _RootScreenState extends State<RootScreen> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Peace Time',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: selectingmode
+              ? IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    setState(() {
+                      selectingmode = false;
+                      paints.forEach((p) => p.selected = false);
+                    });
+                  },
+                )
+              : null,
+          title: Text("Selectable ListView Example"),
+        ),
+        body: ListView(
+          children: List.generate(paints.length, (index) {
+            return ListTile(
+              onLongPress: () {
+                setState(() {
+                  selectingmode = true;
+                });
+              },
+              onTap: () {
+                setState(() {
+                  if (selectingmode) {
+                    paints[index].selected = !paints[index].selected;
+                    print(paints[index].selected.toString());
+                  }
+                });
+              },
+              selected: paints[index].selected,
+              leading: GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {},
+                child: Container(
+                  width: 48,
+                  height: 48,
+                  padding: EdgeInsets.symmetric(vertical: 4.0),
+                  alignment: Alignment.center,
+                  child: CircleAvatar(
+                    backgroundColor: paints[index].colorpicture,
+                  ),
+                ),
+              ),
+              title: Text('ID: ' + paints[index].id.toString()),
+              subtitle: Text(paints[index].title),
+              trailing: (selectingmode)
+                  ? ((paints[index].selected)
+                      ? Icon(Icons.check_box)
+                      : Icon(Icons.check_box_outline_blank))
+                  : null,
+            );
+          }),
+        ),
       ),
-      home: HomeScreen(),
     );
   }
+}
+
+class Paint {
+  final int id;
+  final String title;
+  final Color colorpicture;
+  bool selected = false;
+  Paint(this.id, this.title, this.colorpicture);
 }
 
