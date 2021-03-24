c6f01a5c0f89f3844c69aabf6df80e78a756f956
Sabuj Islam <sabuj0338@gmail.com>
2021-03-24 12:55:47 +0600

    debug mode removed

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class RootScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
